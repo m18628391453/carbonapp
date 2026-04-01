a32258723f24476d3bdcc862d7492fb670ae60dc
zhaozy <Aria_Miazzy@noreply.gitcode.com>
2026-04-01 10:48:40 +0800

    fix: 代码调整，config目录调整；

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,9 +1,5 @@
 // 基础配置表
 export const BASE_CONFIG = {
-    BACKEND: {},
-    API_KEY: {},
-    HOME_TIP_WORDS: [],
-    DEFAULT_ENV: 'ENV_PROD',
     BTN_HIDDEN_TIME: 3000, // 多媒体渲染组件的按钮默认隐藏时间
     LOCK_DURATION: 300, // 前端上锁毫秒数
 }
